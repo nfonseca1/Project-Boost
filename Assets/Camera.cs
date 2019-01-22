2cb976daca8b90eb20a512ed7d57f3b2f9f63a9c
Nathan Fonseca <airide101@gmail.com>
2019-01-22 02:31:18 -0500

    Created camera script for propert camera placement.

---
+++ Assets/Camera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] float distance = 20f;
+    [SerializeField] Transform levelCenter;
+
+    Transform player;
+
+    private void Start()
+    {
+        GetPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            Vector3 midpoint = (levelCenter.position + player.position) / 2;
+            transform.position = new Vector3(
+                midpoint.x,
+                midpoint.y,
+                midpoint.z - distance
+            );
+        }
+        else
+        {
+            GetPlayer();
+        }
+    }
+
+    void GetPlayer()
+    {
+        player = FindObjectOfType<Rocket>().transform;
+    }
+}
